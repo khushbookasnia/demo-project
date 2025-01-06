8eabfbf69cd5bb448747b82b27b0500ffe94d9fc
Khushboo Kasnia <khushboosirsa99@gmail.com>
2025-01-06 14:52:35 +0530

    styles fixed

--- src/components/tasks/task-list-kanban.tsx
+++ src/components/tasks/task-list-kanban.tsx
@@ -2,8 +2,6 @@
 
 import React, { useState } from "react";
 import { Badge } from "../ui/badge";
-import styled, { css } from "styled-components";
-
 import { MoreHorizontalIcon, PlusCircle } from "lucide-react";
 import {
   DragDropContext,
@@ -27,6 +25,20 @@ export default function KanbanBoard() {
   const getTasksByStatus = (status: string): Task[] =>
     tasks.filter((task) => task.status === status);
 
+  const onDragStart = (start: { draggableId: string }) => {
+    const draggedCardElement = document.querySelector(
+      `[data-rbd-drag-handle-draggable-id='${start.draggableId}']`
+    );
+
+    if (draggedCardElement) {
+      const rect = draggedCardElement.getBoundingClientRect();
+      setDraggedCardDimensions({
+        height: rect.height,
+        width: rect.width,
+      });
+    }
+  };
+
   const onDragEnd = (result: DropResult) => {
     const { source, destination } = result;
 
@@ -42,7 +54,7 @@ export default function KanbanBoard() {
   };
 
   return (
-    <DragDropContext onDragEnd={onDragEnd}>
+    <DragDropContext onDragStart={onDragStart} onDragEnd={onDragEnd}>
       <div className="grid grid-cols-4 gap-5 p-4 bg-background rounded-md border">
         {statuses.map((status) => (
           <DroppableColumn
@@ -123,41 +135,15 @@ type TaskCardProps = {
   index: number;
 };
 
-const Issue = styled.div.withConfig({
-  shouldForwardProp: (prop) => prop !== "isBeingDragged",
-})<{ isBeingDragged?: boolean }>`
-  padding: 10px;
-  border-radius: 3px;
-  background: #fff;
-  border: 1px solid #f2f2f2;
-  transition: background 0.1s;
-  cursor: pointer;
-
-  @media (max-width: 1100px) {
-    padding: 10px 8px;
-  }
-
-  &:hover {
-    background: #f5f5f5;
-  }
-
-  ${(props) =>
-    props.isBeingDragged &&
-    css`
-      transform: rotate(3deg);
-      box-shadow: 5px 10px 30px 0px rgba(9, 30, 66, 0.15);
-    `}
-`;
-
 function TaskCard({ task, index }: TaskCardProps) {
   return (
     <Draggable draggableId={String(task.id)} index={index}>
-      {(provided, snapshot) => (
-        <Issue
+      {(provided) => (
+        <div
+          className="p-3 rounded-md border shadow-none dark:bg-zinc-800"
           ref={provided.innerRef}
           {...provided.draggableProps}
           {...provided.dragHandleProps}
-          isBeingDragged={snapshot.isDragging && !snapshot.isDropAnimating}
         >
           <div className="p-0">
             <div className="flex justify-between">
@@ -186,7 +172,7 @@ function TaskCard({ task, index }: TaskCardProps) {
               </Badge>
             )}
           </div>
-        </Issue>
+        </div>
       )}
     </Draggable>
   );
